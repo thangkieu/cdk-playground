26549593299e049577597929c80fd2eabece5884
Thang Kieu <thang.kieu@2359media.com>
2022-08-26 14:26:00 +0800

    Add Stack

--- lib/cdk-playground-stack.ts
+++ lib/cdk-playground-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from 'aws-cdk-lib/pipelines';
 
 export class CdkPlaygroundStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Init a CdkPlaygroundBucket
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+    });
+  }
+}
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPlaygroundQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+export class CdkPlaygroundPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyFirstPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+      }),
+    });
   }
 }
